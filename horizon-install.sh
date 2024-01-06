c4fc1524572b5adfc53621fa768bb564d36d7ec7
PARSICO <39967013+parsico@users.noreply.github.com>
2024-01-06 18:31:30 +0330

    Update horizon-install.sh

--- horizon-install.sh
+++ horizon-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "Enter Yourdomain & Subdomain:" domain
-apt update && apt upgrade -y && apt install curl socat -y && curl https://get.acme.sh | sh && ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt && ~/.acme.sh/acme.sh --register-account -m ssl@parsico.org && ~/.acme.sh/acme.sh --issue -d $domain --standalone && ~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt && bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+apt update && apt upgrade -y && apt install curl socat -y && curl https://get.acme.sh | sh && ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt && ~/.acme.sh/acme.sh --register-account -m ssl@parsico.org && ~/.acme.sh/acme.sh --issue -d $domain --standalone && ~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt && bash <(curl -Ls https://raw.githubusercontent.com/parsico/3x-ui/master/install.sh)
